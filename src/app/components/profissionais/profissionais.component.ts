b2615861ecc35b1c8cd9c5f5f86f89d5fd644e3b
Leandro Alves Maciel Filho <leandro.filho@sis-it.com>
2025-02-04 14:18:48 -0300

    ajustes

--- src/app/components/profissionais/profissionais.component.ts
+++ src/app/components/profissionais/profissionais.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class ProfissionaisComponent {
 
   dadosParaSlider = [
-    { name: 'João Pedro Oliveira Freire', description: 'Neuropsicólogo | CRP 11/18847', photoUrl: 'src/assets/imgs/especialistas/Jp.jpeg' },
+    { name: 'João Pedro Oliveira Freire', description: 'Neuropsicólogo Clínico e Psicoterapeuta | CRP 11/18847', photoUrl: 'src/assets/imgs/especialistas/Jp.jpeg' },
     { name: 'Salinas Freitas', description: 'Desenvolvedora Frontend', photoUrl: 'src/assets/imgs/especialistas/Salinas.jpeg' },
-    { name: 'Mirella', description: 'Gerente de Projetos', photoUrl: 'src/assets/imgs/especialistas/Mirella.jpeg' }
+    { name: 'Mirella de Sousa Oliveira', description: 'Psicóloga  Clínica e Escolar | CRP 11/19015', photoUrl: 'src/assets/imgs/especialistas/Mirella.jpeg' }
   ];
 
 }
