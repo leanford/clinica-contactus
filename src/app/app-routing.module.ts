bb170f0bc128af57af403c951d74fe0de29f659d
Leandro Alves Maciel Filho <leandro.filho@sis-it.com>
2025-02-10 11:37:16 -0300

    att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
-// const routes: Routes = [
-//   { path: '', component: HomeComponent },
-//   { path: '**', redirectTo: '' }
-// ];
-
 const routes: Routes = [
-  { path: '', redirectTo: 'clinica-contactus-web', pathMatch: 'full' },
-  { path: 'clinica-contactus-web', component: HomeComponent },
-  { path: '**', redirectTo: 'clinica-contactus-web' }
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
